6c3d880a3043e90cde1b7e9f7db894273b0149db
rajashree <peddagollarajashree2001@gmail.com>
2023-12-23 23:31:55 +0530

    made a list of activities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./style.css";
+import Header from "./Header/Header.js";
+import Count from "./Count/Count.js";
+import SubHeader from "./Header/SubHeader/SubHeader.js";
+import Items from "./Items.js";
 
-export default function App() {
-  return (
-    <div className="Application">
-      <h1>Aye Captain! 🫡</h1>
-      <h2>Lets create something!</h2>
-    </div>
-  );
-}
+  export default function App() {
+    return (
+      <div className="App">
+        <Header />
+        <SubHeader />
+        
+        <hr />
+  
+        <Count data="10" />
+        <Count data="20"></Count>
+        <hr />
+        <Items />
+        <hr />
+        <Count />
+        <Count label="Counted till" />
+        <hr />
+      </div>
+    );
+  }
---
+++ src/Count/Count.js
@@ -0,0 +1,4 @@
+export default function Count({ data }) {
+    return <h3>Total Count : {data}</h3>;
+  }
+  
