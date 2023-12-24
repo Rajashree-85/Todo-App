2a213aca751d8b0c58dcd160024143801197b783
rajashree <peddagollarajashree2001@gmail.com>
2023-12-24 16:38:02 +0530

    done till checkpoint 3

--- src/App.js
+++ src/App.js
@@ -2,39 +2,33 @@ import "./style.css";
 import Header from "./Header/Header.js";
 import Count from "./Count/Count.js";
 import SubHeader from "./Header/SubHeader/SubHeader.js";
+import { useState } from "react";
 
-const countList = [
-  { id: 0, value: "Read SpringBoot" },
-  { id: 1, value: "Complete assignments" },
-  { id: 2, value: "Prepare breakfast" },
-  { id: 3, value: "Sleep for 2 hours" },
-  {id : 4, value: "Take a shower"}
-];
+ export default function App() {
+  const countList=[
+    { id: 0, value: "Read SpringBoot" },
+    { id: 1, value: "Complete assignments" },
+    { id: 2, value: "Prepare breakfast" },
+    { id: 3, value: "Sleep for 2 hours" },
+    {id : 4, value: "Take a shower"}
+  ];
 
-const listItems = countList.map((num) => (
-  <li key={num.id}>
-    <p>{num.value}</p>
-  </li>
-));
-
-  export default function App() {
-    return (
+    const listItems = countList.map((num) => (
+      <li key={num.id}>
+        <p>{num.value}</p>
+      </li>
+    ));
+  
+      return (
       <div className="App">
         <Header />
         <SubHeader />
-        
-        <hr />
   
-        <Count data="10" />
-        <Count data="20"></Count>
-        
         <hr />
-        <ul>{listItems}</ul>
-        <hr />
+      <ul>{listItems}</ul>
 
-        <Count />
-        <Count label="Counted till" />
-        <hr />
+      <button >Empty</button>
+      
       </div>
     );
   }
