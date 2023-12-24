56bcfe84f73b7ffa3d8951c214ad927bec9629f5
rajashree <peddagollarajashree2001@gmail.com>
2023-12-24 11:01:07 +0530

    Used map function and display todo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import "./style.css";
 import Header from "./Header/Header.js";
 import Count from "./Count/Count.js";
 import SubHeader from "./Header/SubHeader/SubHeader.js";
-import Items from "./Items.js";
+
+const countList = [
+  { id: 0, value: "Read SpringBoot" },
+  { id: 1, value: "Complete assignments" },
+  { id: 2, value: "Prepare breakfast" },
+  { id: 3, value: "Sleep for 2 hours" },
+  {id : 4, value: "Take a shower"}
+];
+
+const listItems = countList.map((num) => (
+  <li key={num.id}>
+    <p>{num.value}</p>
+  </li>
+));
 
   export default function App() {
     return (
@@ -14,9 +27,11 @@ import Items from "./Items.js";
   
         <Count data="10" />
         <Count data="20"></Count>
+        
         <hr />
-        <Items />
+        <ul>{listItems}</ul>
         <hr />
+
         <Count />
         <Count label="Counted till" />
         <hr />
