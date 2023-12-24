88d2a183c7b992fdf026cd4c4dcb2bfcffc059ce
rajashree <peddagollarajashree2001@gmail.com>
2023-12-24 19:07:42 +0530

    vanishing data when clicked empty

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import Header from "./Header/Header.js";
 import Count from "./Count/Count.js";
 import SubHeader from "./Header/SubHeader/SubHeader.js";
 import { useState } from "react";
+import { setTextRange } from "typescript";
 
  export default function App() {
-  const countList=[
+  let [countList,setcountList]=useState([
     { id: 0, value: "Read SpringBoot" },
     { id: 1, value: "Complete assignments" },
     { id: 2, value: "Prepare breakfast" },
     { id: 3, value: "Sleep for 2 hours" },
     {id : 4, value: "Take a shower"}
-  ];
+  ]);
 
-    const listItems = countList.map((num) => (
+  const [TextHidden,setTextHidden]=useState(true);
+  const onClick=()=>setTextHidden(!TextHidden);
+
+
+   let listItems = countList.map((num) => (
       <li key={num.id}>
         <p>{num.value}</p>
       </li>
     ));
   
-      return (
+      return ( 
+
       <div className="App">
         <Header />
-        <SubHeader />
   
         <hr />
-      <ul>{listItems}</ul>
 
-      <button >Empty</button>
+      <button onClick={onClick} id="button" >{TextHidden?'Show':'Empty'}</button>
+      {!TextHidden?<ul>{listItems}</ul>:<SubHeader />}      
       
       </div>
     );
